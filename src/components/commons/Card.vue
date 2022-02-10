19d1d4265da0a2b60dc8fdbef17f6a51a01fde39
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-10 07:46:33 +0100

    Finiti textandimage e card statici

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -1,11 +1,66 @@
 <template>
-  <div></div>
+  <div class="card" :class="{ active: isActive }">
+    <div class="image"><img src="../../assets/img/510.png" alt="" /></div>
+    <div class="text">
+      <div class="title">Reporting Analysis</div>
+      <p>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Card",
+  props: {
+    isActive: Boolean,
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../../assets/style/vars.scss";
+
+.card {
+  display: flex;
+
+  padding: 25px 8px 25px 10px;
+
+  cursor: pointer;
+
+  &.active {
+    border: 1px solid $borderColorSecond;
+    box-shadow: 4.871px 34.659px 30px 0px #f0f0f0;
+
+    position: relative;
+    z-index: 1;
+
+    .title {
+      color: $textColorThird;
+    }
+  }
+}
+
+.image {
+  width: 110px;
+
+  display: flex;
+  align-items: center;
+  img {
+    width: 100%;
+  }
+}
+
+.text {
+  margin-left: 15px;
+  .title {
+    font-family: $fontMain;
+    font-size: 14px;
+    font-weight: bold;
+
+    margin-bottom: 10px;
+  }
+}
+</style>
--- src/components/sections/TextAndImage.vue
+++ src/components/sections/TextAndImage.vue
@@ -1,12 +1,64 @@
 <template>
   <!-- Testo, 2 card (commons), immagine -->
-  <section></section>
+  <section>
+    <div class="container">
+      <div class="text">
+        <h2>Get tips tricks on how to skyrocket your sales.</h2>
+        <p>
+          Faff about only a quid blower I don’t want no agro bleeding chimney
+          pot burke tosser cras nice one boot fanny!
+        </p>
+        <Card id="0" :isActive="selected == 0 ? true : false" />
+        <Card id="1" :isActive="selected == 1 ? true : false" />
+      </div>
+      <div class="image">
+        <img src="../../assets/img/509.jpeg" alt="" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Card from "../commons/Card.vue";
+
 export default {
   name: "TextAndImage",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      selected: 0,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+
+.text {
+  width: 46%;
+
+  h2 {
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-bottom: 35px;
+  }
+}
+
+.image {
+  width: 45%;
+  margin-left: 20px;
+
+  display: flex;
+  align-items: center;
+  img {
+    width: 100%;
+  }
+}
+</style>
