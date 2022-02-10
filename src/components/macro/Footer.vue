ef427aed18b4ebb2f8b1f9ffab33d7416d483578
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-10 11:49:37 +0100

    Footer completato

--- src/components/macro/Footer.vue
+++ src/components/macro/Footer.vue
@@ -1,12 +1,25 @@
 <template>
   <footer>
     <div class="container">
-      <div class="info"></div>
+      <div class="info">
+        <a href="#"><img src="../../assets/img/35.png" alt="Sofbox" /></a>
+        <p>
+          It is a long estabilished fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <div>Copyright @2018 <a href="#">Sofbox</a></div>
+      </div>
       <div class="footer-links">
-        <ul v-for="(category, i) in footerItemsArray" :key="category.categoryName+i">
-          <h4>{{category.categoryName}}</h4>
-          <li v-for="(item, i) in category.links" :key="category.categoryName+item.name+i">
-            <a :href="item.url">{{item.name}}</a>
+        <ul
+          v-for="(category, i) in footerItemsArray"
+          :key="category.categoryName + i"
+        >
+          <h4>{{ category.categoryName }}</h4>
+          <li
+            v-for="(item, i) in category.links"
+            :key="category.categoryName + item.name + i"
+          >
+            <a :href="item.url">{{ item.name }}</a>
           </li>
         </ul>
       </div>
@@ -104,11 +117,52 @@ export default {
 <style lang="scss" scoped>
 @import "../../assets/style/vars.scss";
 
+footer {
+  border-top: 1px solid $borderColorFifth;
+}
+
+.container {
+  padding: 60px 0;
+  display: flex;
+}
+
+.info {
+  width: 210px;
+  margin-right: 25px;
+  font-size: 12px;
+  color: $textColorSecond;
+
+  img {
+    width: 110px;
+  }
+
+  p {
+    padding-bottom: 40px;
+  }
+
+  a {
+    color: $textColorThird;
+  }
+}
+
+.footer-links {
+  display: flex;
+}
 
 ul {
   list-style: none;
-  li a{
-    color: $textColorSecond;
+
+  margin: 0 80px;
+
+  h4 {
+    margin-bottom: 20px;
+  }
+  li {
+    font-size: 12px;
+    margin-bottom: 12px;
+    a {
+      color: $textColorSecond;
+    }
   }
 }
 </style>
